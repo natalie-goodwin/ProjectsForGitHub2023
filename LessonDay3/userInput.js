2bde591ce8dfabd6ffc757eb75a14b57110a8af9
Natalie Goodwin <nmgoodwin27@gmail.com>
2023-05-03 11:18:41 -0500

    added May 3 11:18 am

---
+++ LessonDay3/userInput.js
@@ -0,0 +1,36 @@
+/*input is what makes a computer program 
+valuable; input typically comes from users; 
+this is a temporary method for getting user input */ 
+
+/*  
+var name = window.prompt('what is your name?');
+window.alert(`Welcome, ${name}`);
+*/
+
+/* let username = prompt('Username:');
+let password = prompt('Password:');*/
+/*take these variables and use a conditional 
+to determine if they are correct username and
+password  */
+
+/* 
+if (username == 'paul123' && password == '12345') {
+    alert('Welcome back, ' + username);
+} else {
+    alert('Incorrect username or password.');
+} */ 
+
+
+var loggedIn = false;
+
+while(!loggedIn) {
+    let username = prompt('Username:');
+    let password = prompt('Password:');
+
+if (username == 'paul123' && password == '12345') {
+    alert('Welcome back, ' + username);
+    loggedIn = true; /*when this is true loop will quit iterating */
+} else {
+    alert('Incorrect username or password.');
+    }
+}
